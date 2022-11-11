1a3f10c2e6d518fa2ac0680aaccc780f4a1f8436
stoic <abhishekthory001@gmail.com>
2022-11-12 02:19:04 +0530

    added some functions and created createorder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import {useAuthContext} from "@stardust-platform/web-login";
-
+import { useEffect } from 'react';
+import {apikey} from "./data.js"
+import CreateOrder from './components/CreateOrder';
 
 function App() {
+  
   const { user, handleOpenModal, isOpen, handleSignOut } = useAuthContext();
 
+  const loaddata = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: apikey
+      }
+    };
+    
+    console.log(options);
+
+    fetch('https://marketplace-api.stardust.gg/v1/balance/get', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+
+}
+
+
+  useEffect(() => {
+
+    console.log(user);
+    if (user!==undefined){
+      loaddata()
+    }
+  }, [user])
+  
   return (
     <div >
-            <button onClick={() => handleOpenModal(!isOpen)}>Login</button>
+      {/* <button onClick={() => handleOpenModal(!isOpen)}>Login</button> */}
+
+
+      <CreateOrder />
      
     </div>
   );
